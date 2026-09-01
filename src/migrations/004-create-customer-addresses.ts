20f5aea4afa712d10b12b61f7daf35263f8715cb
Kobe-5623 <fiv5gamein@gmail.com>
2026-09-02 03:27:02 +0800

    Migrations and model of Customer Address fixed incorrect fields

--- src/migrations/004-create-customer-addresses.ts
+++ src/migrations/004-create-customer-addresses.ts
@@ -7,12 +7,7 @@ export async function up({ context }: MigrationContext): Promise<void> {
   await context.createTable('customer_addresses', {
     id: { type: DataTypes.STRING(26), primaryKey: true },
     customer_id: { type: DataTypes.STRING(26), allowNull: false, references: { model: 'customers', key: 'user_id' } },
-    address_1: { type: DataTypes.STRING, allowNull: false },
-    address_2: { type: DataTypes.STRING, allowNull: true },
-    barangay: { type: DataTypes.STRING, allowNull: false },
-    city_municipality: { type: DataTypes.STRING, allowNull: false },
-    province: { type: DataTypes.STRING, allowNull: false },
-    postal_code: { type: DataTypes.STRING(4), allowNull: false },
+    address: { type: DataTypes.STRING, allowNull: false },
     created_at: { type: DataTypes.DATE, allowNull: false },
   });
 }
